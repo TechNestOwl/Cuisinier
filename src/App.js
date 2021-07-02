44adc18fca588c11459049ee8ba820954893317d
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2021-07-02 07:48:10 -0400

    api setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const application_ID = "04f6090b"
+  const API_key = "60ea4ac886e8b3151356975a1a032630	"
   return (
     <div className="App">
-      
+        <h1>React App</h1>
     </div>
   );
 }
