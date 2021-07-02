26149d6b83dd20375111993b50757bc8b314adba
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2021-07-02 08:05:34 -0400

    set up initial state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
 
-  const application_ID = "04f6090b"
-  const API_key = "60ea4ac886e8b3151356975a1a032630	"
+  // const application_ID = "04f6090b"
+  // const API_key = "60ea4ac886e8b3151356975a1a032630	"
+  const API_call = "https://api.edamam.com/api/recipes/v2?type=public&app_id=04f6090b&app_key=60ea4ac886e8b3151356975a1a032630"
+
+  useEffect( () => {
+    console.log("effect has been run");
+  });
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
         <h1>React App</h1>
+        <form className="recipeSearchForm">
+          <input type='text' className="searchBar">
+          </input>
+          <button onClick={() => setCounter(counter + 1)}type="submit" className="searchFormBtn">{counter}</button>
+        </form>
     </div>
   );
 }
